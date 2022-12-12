9dde4341d9b483c4a5d50a5f38ae4b5840bf3a03
MartinKotas <kotasmartin1997@gmail.com>
2022-12-12 18:50:21 +0000

    NavBar

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
