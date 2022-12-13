335921fd4e97d8759d74aafdd1debbaabbbe619c
Muhammadjon-is <isakovw18@gmail.com>
2022-12-13 17:27:12 +0100

    Notification
    Notification Ready

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Notifications from "./Components/Notification/Notifications";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  return <></>;
+  return <>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={
+
+<Notifications/>
+    }/>
+
+  </Routes>
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
